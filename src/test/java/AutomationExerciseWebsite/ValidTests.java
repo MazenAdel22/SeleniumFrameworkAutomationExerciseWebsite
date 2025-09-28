8839b6307e53895aa05f8d72a3fe556c246db9de
MazenAdel <mazn.adel2070@gmail.com>
2025-09-28 23:17:12 +0300

    Second Commit

--- src/test/java/AutomationExerciseWebsite/ValidTests.java
+++ src/test/java/AutomationExerciseWebsite/ValidTests.java
@@ -2,9 +2,7 @@ package AutomationExerciseWebsite;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
