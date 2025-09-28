d6a19791b67f9db5dc65428812fe4663acfd8bb0
MazenAdel <mazn.adel2070@gmail.com>
2025-09-28 23:10:18 +0300

    Initial Commit

---
+++ src/test/java/AutomationExerciseWebsite/InvalidTests.java
@@ -0,0 +1,12 @@
+package AutomationExerciseWebsite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class InvalidTests {
+
+
+}
